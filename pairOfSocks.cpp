b6326483c889618ba562edce3f165f134ae862ad
Brueni <nils.bruenninski@stud.uni-goettingen.de>
2019-10-22 21:05:03 +0200

    first 3 challenges

---
+++ pairOfSocks.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the sockMerchant function below.
+int sockMerchant(int n, vector<int> ar) {
+    map<int, int> sockMap;
+    for(int nr=0; nr < n; ++nr){
+        sockMap[ar[nr]]++;
+    }
+    int pairs = 0;
+    for(auto elem = sockMap.begin() ; elem!=sockMap.end(); ++elem){
+        pairs += elem->second/2;
+    }
+    return pairs;
+}
+
+int main1()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split_string(ar_temp_temp);
+
+    vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = sockMerchant(n, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
